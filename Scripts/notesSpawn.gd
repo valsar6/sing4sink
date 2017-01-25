d164f9ccab047e5a948222fbb9b12eb494c6fbf9
valsar6 <s.dessupoiu1@gmail.com>
2017-01-25 13:50:06 +0100

    master
    modifiche al master

--- Scripts/notesSpawn.gd
+++ Scripts/notesSpawn.gd
@@ -1,14 +1,17 @@
 extends Panel
-
 # class member variables go here, for example:
 # var a = 2
 # var b = "textvar"
 #var attuale = 0
 
+#In questo array i valori corrispondono rispettivamente a 
+# 4/4 2/4 1/4 1/8 1/16 1/32
+#var timing_arr = [1.712, 0.856, 0.428, 0.214, 0.107, 0.053]
+
 func _ready():
-	randomize()
 	var tempo = get_node("tempo")
-	tempo.set_wait_time(3)
+	randomize()
+	tempo.set_wait_time(randi()%3)
 	tempo.start()
 	_spawna()
 	
@@ -16,15 +19,22 @@ func _spawna():
 	var note_scn = load("res://Scenes/nota.tscn")
 	var nota = note_scn.instance()
 	#Varie Texture
-	var tex0 = load("res://Texture/verde.tex")
-	var tex1 = load("res://Texture/blu.tex")
-	var tex2 = load("res://Texture/viola.tex")
-	var tex3 = load("res://Texture/rosso.tex")
-	var tex4 = load("res://Texture/giallo.tex")
+	var tex0 = load("res://Texture/battle_screen/battle_hud/notes/blu.tex")
+	var tex1 = load("res://Texture/battle_screen/battle_hud/notes/rosso.tex")
+	var tex2 = load("res://Texture/battle_screen/battle_hud/notes/giallo.tex")
+	var tex3 = load("res://Texture/battle_screen/battle_hud/notes/viola.tex")
+	var tex4 = load("res://Texture/battle_screen/battle_hud/notes/verde.tex")
+	var group_arr = ["0blu", "1rosso", "2giallo", "3viola", "4verde"]
 	var arr = [tex0,tex1, tex2,tex3,tex4]
+	var num = randi()%4
 	nota.set_pos(Vector2(680, 60))
-	nota.set_texture(arr[randi()%4])
+	nota.set_texture(arr[num])
+	nota.add_to_group(group_arr[num])
+#	print(nota.get_groups())
+#	print(nota.is_in_group("0blu"))
 	add_child(nota)
 func _on_tempo_timeout():
+	var tempo = get_node("tempo")
+	tempo.set_wait_time(randi()%3)
 	_spawna()
 	pass # replace with function body
--- Scripts/playing.gd
+++ Scripts/playing.gd
@@ -3,13 +3,14 @@ extends TextureFrame
 var notifica = 0
 var b = 0
 var attuale = 0
+var serieNote = 0
+var maxTolerance = 120
 func _ready():
 	# Called every time the node is added to the scene.s
 	# Initialization here
 	randomize() 
 	var tempo = get_node("Spawn/tempo")
-	get_node("musica").play("[No Copyright Music] Ukulele Bells - HookSounds")
-	_spawna()
+	get_node("tabellone").add_text(String(serieNote))
 	tempo.set_wait_time(5)
 	tempo.start()
 	
@@ -25,20 +26,101 @@ func _spawna():
 #		var x_coord = randi()%300+1
 #		get_node("nave").set_pos(Vector2(x_coord, 0))
 #		b = 1
+
+#Col timerone posso chiamare la funzione di attacco come mi pare
+func _on_attack_time_timeout():
+	print("Le navi uccise sono ",String(global.killsNavi))
+	_attacco()
+	
+func _attacco():
+	if (serieNote >= 5):
+		var spawn = get_node("Spawn")
+#		var freccia_scn = load("res://Scenes/nota.tscn")
+#		var freccia = freccia.instance()
+#		freccia.set_pos(Vector2(700,950))
+#		add_child(freccia)
+		var nemici = spawn.get_child(2)
+		if (nemici.is_in_group("nemici")  == true && nemici != null):
+			nemici.VITA -= 1
+			
+
 func _tempo():
 	var tempo =get_node("Spawn/tempo")
 	var timer = float(randi()%3+3)
 	tempo.set_wait_time(timer)
 	tempo.start()
-func _on_TouchScreenButton_pressed():
-	#	#how to kill the nave
-	var spawn = get_node("Spawn")
-	var child = spawn.get_child(6)
-	if child != null:
-		child.queue_free()
-
-
-
+	
 func _on_tempo_timeout():
 	_spawna()
 	pass # replace with function body
+
+#Come eliminiamo le figlie di bastarde??
+
+func _on_pesce_pressed():
+	var nota = get_node("sparaNote")
+	var child = nota.get_child(3)
+	if (child.is_in_group("0blu") == true  && child.get_pos().x < maxTolerance && child.get_pos().x > 30 && child != null):
+		get_node("playerone").play("220207__gameaudio__boot-sound")
+		serieNote += 1
+		child.queue_free()
+		_punteggio()
+	else:
+		get_node("playerone").play("220208__gameaudio__click-pop-two-part")
+		serieNote = 0
+		_punteggio()
+func _on_stella_pressed():
+	var nota = get_node("sparaNote")
+	var child = nota.get_child(3)
+	if (child.is_in_group("1rosso") == true  && child.get_pos().x < maxTolerance && child.get_pos().x > 30 && child != null):
+		get_node("playerone").play("220207__gameaudio__boot-sound")
+		serieNote += 1
+		child.queue_free()
+		_punteggio()
+	else:
+		get_node("playerone").play("220208__gameaudio__click-pop-two-part")
+		serieNote = 0
+		_punteggio()
+func _on_conchiglia_pressed():
+	var nota = get_node("sparaNote")
+	var child = nota.get_child(3)
+	if (child.is_in_group("2giallo") == true  && child.get_pos().x < maxTolerance && child.get_pos().x > 30 && child != null):
+		get_node("playerone").play("220207__gameaudio__boot-sound")
+		serieNote += 1
+		child.queue_free()
+		_punteggio()
+	else:
+		get_node("playerone").play("220208__gameaudio__click-pop-two-part")
+		serieNote = 0
+		_punteggio()
+func _on_perla_pressed():
+	var nota = get_node("sparaNote")
+	var child = nota.get_child(3)
+	print(String(child.get_pos()))
+	if (child.is_in_group("3viola") == true && child.get_pos().x < maxTolerance && child.get_pos().x > 30  && child != null):
+		get_node("playerone").play("220207__gameaudio__boot-sound")
+		serieNote += 1
+		child.queue_free()
+		_punteggio()
+	else:
+		get_node("playerone").play("220208__gameaudio__click-pop-two-part")
+		serieNote = 0
+		_punteggio()
+
+func _on_alga_pressed():
+	var nota = get_node("sparaNote")
+	var child = nota.get_child(3)
+	if (child.is_in_group("4verde") == true  && child.get_pos().x < maxTolerance && child.get_pos().x > 30 && child != null):
+		get_node("playerone").play("220207__gameaudio__boot-sound")
+		serieNote += 1
+		child.queue_free()
+		_punteggio()
+	else:
+		get_node("playerone").play("220208__gameaudio__click-pop-two-part")
+		serieNote = 0
+		_punteggio()
+
+func _punteggio():
+	get_node("tabellone").clear()
+	get_node("tabellone").add_text(String(serieNote))
+
+
